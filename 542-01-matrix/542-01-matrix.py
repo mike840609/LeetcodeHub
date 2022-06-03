a76eb8566e1889175f39ebc825030aaf381e8218
Mike Tsai <mike840609@gmail.com>
2022-06-03 18:30:38 +0800

    Time: 743 ms (73.47%), Space: 17.1 MB (70.75%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        DIR = [0, 1, 0, -1, 0]
+        
+        q = deque([])
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0 :
+                    q.append((r,c))
+                else:
+                    mat[r][c] = -1 # not processed yet!
+                    
+        
+        while q :
+            r, c = q.popleft()
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i+1]
+                
+                if 0 <= nr < m and 0<= nc < n and mat[nr][nc] == -1:
+                    mat[nr][nc] = mat[r][c] + 1
+                    q.append((nr, nc))
+        return mat
+            
+        
