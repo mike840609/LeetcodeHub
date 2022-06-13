1f1023d83ff860b36e4b4cf6f7d1f933ca764826
Mike Tsai <mike840609@gmail.com>
2022-06-14 02:14:19 +0800

    Time: 1040 ms (93.27%), Space: 14.3 MB (49.02%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,12 +1,9 @@
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [sys.maxsize] * (amount+1)
-        dp[0] = 0
-        
-        for c in sorted(coins, reverse = True):
-            for i in range(c, len(dp)):
-                dp[i] = min(dp[i], dp[i-c]+1)
-            
-        return dp[-1] if dp[-1] != sys.maxsize else -1
-            
-            
+class Solution(object):
+    def coinChange(self, coins, amount):
+        MAX = float('inf')
+        dp = [0] + [MAX] * amount
+
+        for i in range(1, amount + 1):
+            dp[i] = min([dp[i - c] if i - c >= 0 else MAX for c in coins]) + 1
+
+        return [dp[amount], -1][dp[amount] == MAX]
