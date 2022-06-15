54e082a4779621b619324846c64da74f044e96fb
Mike Tsai <mike840609@gmail.com>
2022-06-16 00:43:41 +0800

    Time: 696 ms (23.41%), Space: 13.9 MB (71.72%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.py
+++ 650-2-keys-keyboard/650-2-keys-keyboard.py
@@ -7,7 +7,11 @@ class Solution:
         
         for i in range(1, n+1):
             for j in range(i-1, 0, -1):
+                # if sequence of length 'j' can be pasted multiple times to get length 'i' sequence
                 if i%j == 0:
+                    # we just need to paste sequence j (i/j - 1) times, hence additional (i/j) times since we need to copy it first as well.
+                    # we don't need checking any smaller length sequences
+                    # here: actually should be +1(copy) -1(original part)
                     dp[i] = dp[j] + i//j
                     break
                     
