56dc8a9cb97c203e55004f74fb516c277510c2ee
Mike Tsai <mike840609@gmail.com>
2022-06-21 00:15:28 +0800

    Time: 193 ms (77.21%), Space: 14.5 MB (76.28%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.py
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -1,23 +1,9 @@
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        trie = {}
-        
-        for word in words:
-            p = trie
-            for c in word[::-1]:
-                p.setdefault(c, {})
-                p = p[c]            
-        
-        
-        self.res = 0        
-        def dfs(root, i):
-            if not root:
-                self.res += (i+1)
-                return 
+        s = set(words)
+        for w in words:
+            for i in range(1, len(w)):
+                s.discard(w[i:])
+        return sum([len(x)+1 for x in s])
                 
-            for k, v in root.items():
-                dfs(v, i+1)
-        
-        dfs(trie, 0)
-        return self.res 
                 
