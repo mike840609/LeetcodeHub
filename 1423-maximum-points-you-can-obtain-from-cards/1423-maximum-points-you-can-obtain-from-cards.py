418088016c08e4da9f9451d1f7ddc085ea24bc42
Mike Tsai <mike840609@gmail.com>
2022-06-26 14:50:12 +0800

    Time: 912 ms (5.40%), Space: 27.5 MB (17.27%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,16 +1,25 @@
+'''
+what is remove from head or tail  `n` times to make it sum maximum means?
+
+=> the sum of remain subArray must be minimum
+
+=> using `prefix sum` and `sliding windows` to solve this problem
+
+'''
 class Solution:
     def maxScore(self, A: List[int], k: int) -> int:
-        preSum = [0] + list(accumulate(A))        
+        
+        preSum = [0] + list(accumulate(A))                
         
         min_ = sys.maxsize
+        
         winSize = len(A) - k 
         
         if k == len(A): 
             return preSum[-1]
-        
-        # print(preSum)
-        for i in range(len(preSum) - winSize):            
-            # print(i+winSize, i)
+           
+        for i in range(len(preSum) - winSize):              
             min_ = min(min_, preSum[i+winSize] - preSum[i])            
             
         return sum(A) - min_ 
+    
