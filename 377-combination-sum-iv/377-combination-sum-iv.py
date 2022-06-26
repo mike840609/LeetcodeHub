480402b6d6af32503d018d6b10193cd09b3ae7f1
Mike Tsai <mike840609@gmail.com>
2022-06-26 17:54:02 +0800

    Time: 295 ms (5.08%), Space: 14.1 MB (29.59%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,28 +1,19 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
-        cnt = {0:1}
-        
+        cnt = {0:1}        
         res = 0 
+        
         while cnt:            
             tmp = defaultdict(int)
-            flag = False 
             for k, v in cnt.items():
                 for n in nums:
                     if k+n <= target:
-                        tmp[k+n] += v
-                        flag = True 
+                        tmp[k+n] += v                        
 
             cnt = tmp            
             res += cnt.get(target, 0)
             
         return res 
-            
-                
-            
-        
-                    
-            
-            
                 
         
