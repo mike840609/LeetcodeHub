ab01eb18332ae4cc432eb8d933c3fdc6436f0d2d
Mike Tsai <mike840609@gmail.com>
2022-06-27 01:11:40 +0800

    Time: 514 ms (79.68%), Space: 14.8 MB (71.00%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        s = sum(nums)
+        if s&1: return False 
+                
+        target = s//2
+        
+        dp = [0] * (target+1)
+        
+        dp[0] = 1
+    
+        seen = {0}
+        
+        for n in nums:
+            tmp = set()
+            for k in seen:
+                if k+n < target:
+                    tmp.add(k+n)
+                elif k+n == target:
+                    return True
+                
+            seen |= tmp
+            
+        return False 
+                
+            
