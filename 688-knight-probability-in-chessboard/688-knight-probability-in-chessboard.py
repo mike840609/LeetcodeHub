3f44efd9e390007e35f9642efef0d9928edf9f7d
Mike Tsai <mike840609@gmail.com>
2022-06-26 22:05:43 +0800

    Time: 405 ms (55.60%), Space: 24.7 MB (10.94%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
@@ -1,13 +1,10 @@
 class Solution:
-    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        total = n* n
-        dp = [[0] * n for _ in range(n)]
-        
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:        
+
         pos = [[-1, -2], [-1, +2], [-2, -1], [-2, 1], [1, -2], [1, +2], [2, -1], [2, +1]]                
-        
-        
+                
         @lru_cache(None)
-        def dfs(i, j, k):                        
+        def dp(i, j, k):                        
             
             if k == 0 :
                 return 1
@@ -15,9 +12,9 @@ class Solution:
             cnt = 0
             for x, y in pos:
                 if 0 <= i+x < n and 0 <= j+y < n :
-                    cnt += dfs(i+x, j+y, k-1)                                                                                    
+                    cnt += dp(i+x, j+y, k-1)                                                                                    
             return cnt                 
                     
-        return dfs(row, column, k) / (8**k)
+        return dp(row, column, k) / (8**k)
                     
             
