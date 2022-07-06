ce2b70fa4238d5c8a0123a934fdd74cecb5ad43f
Mike Tsai <mike840609@gmail.com>
2022-07-07 02:06:38 +0800

    Time: 38 ms (73.80%), Space: 13.8 MB (58.54%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,15 +1,10 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        @lru_cache(None)
-        def dp(n):
-            if n <= 1:
-                return 1
-            elif n == 2:
-                return 2 
-            
-            res = 0 
-            for i in range(n):                
-                res += dp(i) * dp(n-i-1)
-            return res 
+        dp = [0] * (n+1)
+        dp[0] = 1 
         
-        return dp(n)
+        for i in range(1, n+1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i-j-1]
+        
+        return dp[n]
