a5c9c6a97f811f02baabb0b37a212d68fc4824af
Mike Tsai <mike840609@gmail.com>
2022-07-16 02:58:13 +0800

    Time: 75 ms (41.53%), Space: 14.1 MB (96.87%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1 : return 0 
+        
+        buy = [0] * len(prices)
+        sell = [0] * len(prices)        
+        buy[0] = -prices[0]
+        
+        for i in range(1, len(prices)):
+            if i == 1:                
+                buy[i] = max(buy[i-1], -prices[i])
+            else:
+                buy[i] = max(buy[i-1], sell[i-2] - prices[i])
+            
+            # keep the same as day i-1, or sell from buy status at day i-1
+            sell[i] = max(sell[i-1], buy[i-1] + prices[i])
+            
+        return sell[-1]
