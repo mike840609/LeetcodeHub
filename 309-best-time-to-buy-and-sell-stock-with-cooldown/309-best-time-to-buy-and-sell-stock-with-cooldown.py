782c5ebc0e755c828972b88eb3bc28d1e2832de2
Mike Tsai <mike840609@gmail.com>
2022-07-16 16:31:52 +0800

    Time: 37 ms (98.45%), Space: 14.1 MB (96.87%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,3 +1,16 @@
+'''
+init state
+1. hold = float('-inf')[can not start from this state]
+2. not_hold = 0, [can start from this state]
+3. not_hold_cooldown = float('-inf')[can not start from this state]
+
+state machine:
+1. hold -----do nothing----->hold
+2. hold -----sell----->notHold_cooldown
+3. notHold -----do nothing -----> notHold
+4. notHold -----buy-----> hold
+5. notHold_cooldown -----do nothing----->notHold
+'''
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         hold, not_hold, not_hold_cooldown = float('-inf'), 0, float('-inf')
