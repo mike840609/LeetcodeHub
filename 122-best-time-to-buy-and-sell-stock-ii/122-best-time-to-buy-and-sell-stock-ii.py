ae57d3937b8093a4d26d878a97d028df06da21ed
Mike Tsai <mike840609@gmail.com>
2022-07-17 18:16:09 +0800

    Time: 110 ms (34.33%), Space: 15.2 MB (68.74%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        not_hold, hold = 0, float('-inf')
-        
-        for p in prices:
-            not_hold, hold = max(not_hold, hold+p), max(hold, not_hold-p)
-        
-        return not_hold
+        res = 0 
+        for i in range(1, len(prices)):
+            res += max(prices[i] - prices[i-1], 0)
+        return res
