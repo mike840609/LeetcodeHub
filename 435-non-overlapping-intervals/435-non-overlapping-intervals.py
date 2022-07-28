ff525cb28cdc10872c46960131610419e58190dc
Mike Tsai <mike840609@gmail.com>
2022-07-29 02:50:38 +0800

    Time: 2718 ms (15.53%), Space: 52.8 MB (39.50%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -3,7 +3,7 @@ class Solution:
         
         end, res = float("-inf"), 0                 
         # print(sorted(intervals, key=lambda x: (x[1], -x[0])))
-        for x, y in sorted(intervals, key=lambda x: (x[1], -x[0])):            
+        for x, y in sorted(intervals, key=lambda x: (x[1])):            
             if x < end :
                 res += 1            
             else:
