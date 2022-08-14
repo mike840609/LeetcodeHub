393240c8fc7a3fd81faba5e47c3501c377540534
Mike Tsai <mike840609@gmail.com>
2022-08-15 01:13:49 +0800

    Time: 33 ms (100.00%), Space: 13.8 MB (100.00%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
@@ -1,36 +1,13 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        self.res = []
-        
-        def dfs(i, s, tmp):
-            if self.res:
-                return 
-            if i >= len(pattern):
-                self.res.append(s)
-                return
-            
-            if pattern[i] == 'D':
-                for c in tmp:
-                    if c < int(s[-1]):
-                        tmp.remove(c)
-                        dfs(i+1, s+str(c), tmp)
-                        tmp.add(c)
-
-                    
-            if pattern[i] == 'I':
-                for c in tmp:
-                    if c > int(s[-1]):
-                        tmp.remove(c)
-                        dfs(i+1, s+str(c), tmp)
-                        tmp.add(c)
-
-        for i in range(1, 10):
-            if not self.res :
-                s = set([1,2,3,4,5,6,7,8,9])
-                s.remove(i)
-                dfs(0, str(i), s)
-        
-        return sorted(self.res)[0]
+        res, stk = [], []        
+        for i, val in enumerate(pattern + 'I', 1):
+            stk.append(str(i))
+            if val == 'I':
+                res += stk[::-1]
+                stk = [] 
+        return ''.join(res)
+                
             
                 
                 
