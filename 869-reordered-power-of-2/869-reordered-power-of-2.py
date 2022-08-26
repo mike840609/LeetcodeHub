e6e7effb61a988b9aee53a373e16e75fc4ac24e6
Mike Tsai <mike840609@gmail.com>
2022-08-26 12:43:56 +0800

    Time: 48 ms (73.80%), Space: 13.8 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,3 +1,8 @@
 class Solution:
-    def reorderedPowerOf2(self, N: int) -> bool:
-        return sorted(str(N)) in [sorted(str(1 << i)) for i in range(30)]
+    def reorderedPowerOf2(self, n: int) -> bool:
+        # 2^n < 10**9, n smaller than 30, no need to use set 
+        res = []                                    
+        for i in range(30):
+            res.append(str(sorted(str(1 << i))))
+        
+        return str(sorted(str(n))) in res
