ae797cc38b7aad1f3acadce5bf8ba789f9933085
Mike Tsai <mike840609@gmail.com>
2022-09-03 14:51:58 +0800

    Time: 40 ms (94.90%), Space: 14.3 MB (41.16%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,21 +1,19 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
         num_list = list(str(x) for x in range(10))
-        def dfs(i):
+        dp = defaultdict(set)
+        for i in range(10):
+            dp[i].add(str(i))
             
-            if i == n:
-                return num_list
-            
-            post = dfs(i+1)
-            
-            tmp = []            
-            for j in num_list:
-                for s in post:
-                    if abs(ord(j)-ord(s[0])) == k:
-                        tmp.append(j + s)            
-            return tmp 
-        
-        res = dfs(1)
-        
-        return [''.join(x) for x in res if x[0] != '0']
+        for _ in range(n-1):
+            tmp = defaultdict(set)
+            for i in range(10):
+                for l in dp[i+k]:
+                    tmp[i].add(str(i)+l)
+                for l in dp[i-k]:
+                    tmp[i].add(str(i)+l)            
+            dp = tmp
                 
+        return [x for l in dp.values() for x in l if x[0] != '0']
+            
+            
