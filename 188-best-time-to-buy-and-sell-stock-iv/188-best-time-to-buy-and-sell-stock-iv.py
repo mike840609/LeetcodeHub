512b846e28e4b861247c7b250612fcff058def8c
Mike Tsai <mike840609@gmail.com>
2022-09-10 13:49:18 +0800

    Time: 232 ms (44.54%), Space: 13.9 MB (95.05%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,19 +1,19 @@
 class Solution:
-    def maxProfit(self, k: int, prices: List[int]) -> int:
-        if k == 0 :
-            return 0 
+    def maxProfit(self, k: int, prices: List[int]) -> int:        
         # [buy, sell]
-        dp = [[float('-inf'), 0] for _ in range(k)]
-        for p in prices:
+        
+        if k == 0: return 0        
+        dp = [[-float('inf'), 0] for _ in range(k) ] 
+        
+        for p in prices :
             for i in range(k):
-                if i == 0:
-                    dp[i][0] = max(dp[i][0], -p)                
+                if i == 0 :
+                    dp[i][0] = max(dp[i][0], -p)
                     dp[i][1] = max(dp[i][1], dp[i][0] + p)
                 
                 else:
-                    dp[i][0] = max(dp[i][0], dp[i-1][1] - p)                 
-                    dp[i][1] = max(dp[i][1], dp[i][0] + p )
-        # print(dp)
+                    dp[i][0] = max(dp[i][0], dp[i-1][1] - p)
+                    dp[i][1] = max(dp[i][1], dp[i][0] + p)
         return dp[-1][1]
                 
         
