9b4484a84726440149e46a7b95dd9a6d13fcc93b
Mike Tsai <mike840609@gmail.com>
2022-09-17 14:28:19 +0800

    Time: 90 ms (11.87%), Space: 14.2 MB (90.84%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0 , len(nums) - 1
 
-        while l <= r :
+        while l < r :
             m = (l+r) // 2 
             
             if nums[m] == target:
@@ -10,12 +10,13 @@ class Solution:
 
             if  nums[l] <= nums[m]:
                 if nums[l] <= target <= nums[m]:
-                    r = m -1 
+                    r = m 
                 else :
                     l = m + 1                 
             else:
                 if nums[m] <= target <= nums[r]:
                     l = m + 1 
                 else : 
-                    r = m -1                                     
-        return -1
+                    r = m
+        
+        return l if nums[l] == target else -1
