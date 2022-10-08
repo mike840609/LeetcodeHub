3baf2b33f7d2c410da11b4aa4e26ea6379c1767b
Mike Tsai <mike840609@gmail.com>
2022-10-09 00:04:40 +0800

    Time: 234 ms (98.51%), Space: 14.2 MB (53.95%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,29 +1,26 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        
-        res = float('inf')
-        diff = float('inf')
-        
-        for i in range(len(nums)-2):
-            j = i + 1
-            k = len(nums) - 1
-                        
-            while j < k :
-                d = ((nums[i] + nums[j] + nums[k]) - target)
-                abs_d = abs(d)
-                if d < 0:
-                    if abs_d < diff:
-                        res = d + target
-                        diff = abs_d
-                    j += 1
-                elif d > 0:                    
-                    if abs_d < diff:
-                        res = d + target
-                        diff = abs_d
-                    k -= 1
+        if target >= sum(nums[-3:]):
+            return sum(nums[-3:])
+        ans = sum(nums[:3])
+        for i in range(len(nums)):
+            if i < len(nums) - 2:
+                idx = bisect_left(nums, (target - nums[i]) // 2, lo=i + 1)
+                if idx == len(nums):
+                    ans = nums[i] + nums[-1] + nums[-2]
+                    continue
+                if nums[idx] < (target - nums[i]) // 2:
+                    lp, rp = idx - 1, idx
                 else:
-                    return target
-        return res
-                
-            
+                    lp, rp = idx, idx + 1
+                while lp > i and rp < len(nums):
+                    if abs(target - nums[i] - nums[lp] - nums[rp]) < abs(target - ans):
+                        ans = nums[i] + nums[lp] + nums[rp]
+                    if nums[i] + nums[lp] + nums[rp] > target:
+                        lp -= 1
+                    elif nums[i] + nums[lp] + nums[rp] < target:
+                        rp += 1
+                    else:
+                        return ans
+        return ans
