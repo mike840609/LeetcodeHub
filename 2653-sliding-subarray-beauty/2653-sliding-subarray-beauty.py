f9fa7b574250cf56cc6e286370fb545036d4673b
Mike Tsai <mike840609@gmail.com>
2023-04-23 17:53:49 +0800

    Time: 8685 ms (12.50%), Space: 31.9 MB (12.50%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
@@ -1,18 +1,29 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+
+        dp = Counter()                
         res = [] 
-        l = SortedList()
-        
-        for i, v in enumerate(nums):
-            l.add(v)
+        for idx, n in enumerate(nums):
+            pre_idx = idx - k
+            dp[n] += 1
+            if pre_idx >= 0 and idx >= k:
+                dp[nums[pre_idx]] -= 1
+                if dp[nums[pre_idx]] == 0 :
+                    del dp[nums[pre_idx]]
             
-            if len(l) > k: l.remove(nums[i-k])
-            
-            if i >= k-1: res.append(min(0, l[x-1]))
+            if idx >= k - 1:
+                cnt = 0 
+                for i in range(-50, 51):
+                    cnt += dp[i]                     
+                    if cnt >= x:
+                        break
                         
-        
+
+                res.append(min(i, 0))
+
+
+
+                    
+                        
         return res
-            
-            
+                
