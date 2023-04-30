9fbab4f88c2d0a2073076ba96fac999087969732
Mike Tsai <mike840609@gmail.com>
2023-05-01 01:17:33 +0800

    Time: 96 ms (44.75%), Space: 23.1 MB (5.51%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,7 +1,7 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
-        res = []
-        for i in range(n+1):
-            res.append(f'{i:b}'.count('1'))
-        
-        return res 
+    def countBits(self, num: int) -> List[int]:
+        counter = [0]
+        for i in range(1, num+1):
+            # print(i, i>>1)
+            counter.append(counter[i >> 1] + i % 2)
+        return counter
