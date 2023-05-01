5a31432c8b54bb9bc72daa5b528ea198217b5440
Mike Tsai <mike840609@gmail.com>
2023-05-01 14:33:37 +0800

    Time: 154 ms (23.40%), Space: 17.8 MB (5.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        if len(nums) == 0 : return 0 
-        s = sum(nums)
-        s1 = ((1 + len(nums)) * len(nums)) // 2        
-        return s1 - s
+        xor = 0 
+        for i, n in enumerate(nums):            
+            xor = xor ^ (i+1) ^ n 
+        return xor 
                 
         
