6b36c726dbad9ad6520f3e29ffaec7473096fe40
Mike Tsai <mike840609@gmail.com>
2023-05-14 22:59:14 +0800

    Time: 129 ms (76.11%), Space: 17.5 MB (15.93%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1: return 0 
+        
+        l, r = 0, nums[0]
+        res = 1 
+        
+        while r < len(nums) - 1: 
+            res += 1
+            nxt = max(i + nums[i] for i in range(l, r+1))            
+            l, r = r, nxt         
+        return res
