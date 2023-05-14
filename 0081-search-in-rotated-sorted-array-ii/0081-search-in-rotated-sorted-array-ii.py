bc9630bd4a07d28bd2329fbf72b803d033cce87c
Mike Tsai <mike840609@gmail.com>
2023-05-14 16:53:37 +0800

    Time: 73 ms (7.63%), Space: 16.9 MB (7.30%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        l, r = 0, len(nums)-1        
+        
+        while l <= r:
+            m = (l+r) // 2 
+            if nums[m] == target: 
+                return True 
+            
+            # remove duplicate num
+            while l < m and nums[l] == nums[m]:
+                l += 1 
+            
+            if nums[l] <= nums[m]:
+                if nums[l] <= target <= nums[m]:
+                    r = m
+                else:
+                    l = m + 1
+                    
+            else:
+                if nums[m] <= target <= nums[r]:
+                    l = m + 1 
+                else:
+                    r = m
+                    
+        return False
