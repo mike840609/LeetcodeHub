5f9491f76162ab456c1268e326bb16c486182100
Mike Tsai <mike840609@gmail.com>
2023-05-21 00:54:34 +0800

    Time: 100 ms (20.36%), Space: 20 MB (13.28%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,10 +1,19 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         bisect.insort(intervals, newInterval)
+        
+        s, e = newInterval
+        
         res = [] 
-        for i, (x,y) in enumerate(intervals):            
-            if res and x <= res[-1][1]:
-                res[-1][1] = max(res[-1][1], y)
+        
+        for i, (x, y) in enumerate(intervals):
+            if x > e:
+                res += intervals[i:]
+                return res 
+            elif res and x <= res[-1][-1]:
+                res[-1][-1] = max(res[-1][-1], y)
+            
             else:
                 res.append([x,y])
         return res
+                
