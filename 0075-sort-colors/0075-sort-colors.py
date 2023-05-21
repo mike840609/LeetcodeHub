ba998fe4ce3fe7bf02a36950cb490624d1e22040
Mike Tsai <mike840609@gmail.com>
2023-05-21 17:28:24 +0800

    Time: 50 ms (21.68%), Space: 16.4 MB (7.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,22 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        b = len(nums)-1
-        r,w = 0, 0
+        # 012, red,white,blue
         
+        r, w, b = 0, 0, len(nums)-1
         
         while w <= b:
-            if nums[w] == 0 :
-                nums[r], nums[w] = nums[w], nums[r]
+            if nums[w] == 0:
+                nums[w], nums[r] = nums[r], nums[w]
                 r += 1
                 w += 1
                 
-            elif nums[w] == 2:
-                nums[w], nums[b] = nums[b], nums[w]
-                b -= 1
-                            
-            elif nums[w] == 1:
+            elif nums[w] == 1:                
                 w += 1
+                            
+            elif nums[w] == 2:
+                nums[b], nums[w] = nums[w], nums[b]                
+                b -= 1
                 
-        
-            
+                                                                
