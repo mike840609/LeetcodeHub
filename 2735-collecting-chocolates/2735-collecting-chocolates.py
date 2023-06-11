2d994b8cd864a0ee4571d63c4726095919377bc9
Mike Tsai <mike840609@gmail.com>
2023-06-11 17:10:33 +0800

    Time: 3660 ms (42.86%), Space: 16.6 MB (7.14%) - LeetHub

--- 2735-collecting-chocolates/2735-collecting-chocolates.py
+++ 2735-collecting-chocolates/2735-collecting-chocolates.py
@@ -3,11 +3,15 @@ class Solution:
         n = len(nums)
         res = [i*x for i in range(len(nums))]
         
+        # type i
         for i in range(n):
             cur = nums[i]
+            
+            # rotatek k
             for k in range(n):
-                cur = min(cur, nums[i-k])
                 
+                cur = min(cur, nums[i-k])                
                 res[k] += cur
+                        
         
         return min(res)
