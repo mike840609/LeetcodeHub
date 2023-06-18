8f5429773126bd4fdfa6ced9d0382fa24bd17f6d
Mike Tsai <mike840609@gmail.com>
2023-06-18 15:29:00 +0800

    Time: 3922 ms (33.33%), Space: 217.9 MB (66.67%) - LeetHub

---
+++ 2741-special-permutations/2741-special-permutations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def specialPerm(self, nums: List[int]) -> int:
+        n, MOD = len(nums), 10**9 + 7
+        @cache
+        def dfs(prev, mask):
+            if mask == (1 << n) - 1: return 1
+            count = 0
+            for i in range(n):
+                if not (mask & (1 << i)) and (nums[i] % prev == 0 or prev % nums[i] == 0):
+                    count += dfs(nums[i], mask | (1 << i))
+            return count % MOD
+        return dfs(1, 0)
+        
