9c6b6ea69e35aca09da7cb4bbdb807f4da7195e3
Mike Tsai <mike840609@gmail.com>
2023-07-23 16:18:01 +0800

    Time: 1109 ms (75.00%), Space: 31.4 MB (75.00%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.py
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.py
@@ -5,16 +5,14 @@ class Solution:
         odd_dp = 0
         even_dp = 0
         
-        for i in range(len(nums)-1, 0, -1):
+        for i in range(len(nums)-1, -1, -1):
             if nums[i] & 1:
                 odd_dp = max(even_dp + nums[i] - x, odd_dp + nums[i])                
                 
             elif nums[i] & 1 == 0:
-                even_dp = (max(odd_dp + nums[i] - x, even_dp + nums[i]))
+                even_dp = max(odd_dp + nums[i] - x, even_dp + nums[i])
                 
-        if nums[0] & 1 :
-            return nums[0] + max(odd_dp, even_dp - x)
-        elif nums[0] & 1 == 0: 
-            return nums[0] + max(even_dp, odd_dp - x)
+        
+        return odd_dp if nums[0] & 1 else even_dp
         
         
