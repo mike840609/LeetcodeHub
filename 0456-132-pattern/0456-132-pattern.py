d24ca4dc664968df466c97f409456992ab3e9cc4
Mike Tsai <mike840609@gmail.com>
2023-09-30 13:11:34 +0800

    Time: 395 ms (72.00%), Space: 36.6 MB (23.86%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,24 +1,15 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         # 231 from right to left 
-        stk = []         
-        s = -float('inf')
+    
+        # increase monotonic stack 
+        stk, third = [], float('-inf')        
         
-        for n in nums[::-1]:
-            if not stk:
-                stk.append(n)
-            
-            if n < s:
-                return True 
-            
+        for n in nums[::-1]:            
+            if n < third:
+                return True             
             while stk and stk[-1] < n :
-                s = stk.pop()
-            
-            stk.append(n)
-        
-        return False 
-        
-        
-        
-        
+                third = stk.pop()            
+            stk.append(n)        
+        return False                                 
         
