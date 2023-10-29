d6e33f233afa423686d7254f0165d3837befec4f
Mike Tsai <mike840609@gmail.com>
2023-10-29 23:32:36 +0800

    Time: 2463 ms (28.31%), Space: 51.9 MB (13.73%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,76 +1,112 @@
-class TreeNode:
-    def __init__(self, start, end, sum, left= None, right=None):
+"""
+    The idea here is to build a segment tree. Each node stores the left and right
+    endpoint of an interval and the sum of that interval. All of the leaves will store
+    elements of the array and each internal node will store sum of leaves under it.
+    Creating the tree takes O(n) time. Query and updates are both O(log n).
+"""
+
+#Segment tree node
+class Node(object):
+    def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.sum = sum
-        self.left = left
-        self.right = right
-
-class NumArray:
-
-    def __init__(self, nums: List[int]):
+        self.total = 0
+        self.left = None
+        self.right = None
         
-        def build(start, end, vals):
-            if start > end:
-                return None
 
-            if start == end:
-                return TreeNode(start, end, vals[start])
-
-            m = (start + end) // 2
-            left = build(start, m, vals)
-            right = build(m+1, end, vals)
-            return TreeNode(start, end, left.sum + right.sum, left, right)
+class NumArray(object):
+    def __init__(self, nums):
+        #helper function to create the tree from input array
+        def createTree(nums, l, r):
             
-        self.root = build(0, len(nums)-1, nums)
-    
-
-    def update(self, index: int, val: int) -> None:
-        def update_(root, index, val):
-            if root.start == root.end == index:
-                root.sum = val 
+            #base case
+            if l > r:
+                return None
+                
+            #leaf node
+            if l == r:
+                n = Node(l, r)
+                n.total = nums[l]
+                return n
+            
+            mid = (l + r) // 2
+            
+            root = Node(l, r)
+            
+            #recursively build the Segment tree
+            root.left = createTree(nums, l, mid)
+            root.right = createTree(nums, mid+1, r)
+            
+            #Total stores the sum of all leaves under root
+            #i.e. those elements lying between (start, end)
+            root.total = root.left.total + root.right.total
+                
+            return root
+        
+        self.root = createTree(nums, 0, len(nums)-1)
+            
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        #Helper function to update a value
+        def updateVal(root, i, val):
+            
+            #Base case. The actual value will be updated in a leaf.
+            #The total is then propogated upwards
+            if root.start == root.end:
+                root.total = val
                 return val
-            m = (root.start + root.end) // 2
-            if index <= m:
-                update_(root.left, index, val)
+        
+            mid = (root.start + root.end) // 2
+            
+            #If the index is less than the mid, that leaf must be in the left subtree
+            if i <= mid:
+                updateVal(root.left, i, val)
+                
+            #Otherwise, the right subtree
             else:
-                update_(root.right, index, val)
+                updateVal(root.right, i, val)
+            
+            #Propogate the changes after recursive call returns
+            root.total = root.left.total + root.right.total
+            
+            return root.total
+        
+        return updateVal(self.root, i, val)
 
-            root.sum = root.left.sum + root.right.sum
-
-        return update_(self.root, index, val)
-
-
-    def sumRange(self, i, j) -> int:
-        def sumRange_(root, i, j):
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        #Helper function to calculate range sum
+        def rangeSum(root, i, j):
+            
+            #If the range exactly matches the root, we already have the sum
             if root.start == i and root.end == j:
-                return root.sum
+                return root.total
             
             mid = (root.start + root.end) // 2
             
+            #If end of the range is less than the mid, the entire interval lies
+            #in the left subtree
             if j <= mid:
-                return sumRange_(root.left, i, j)
-
+                return rangeSum(root.left, i, j)
+            
+            #If start of the interval is greater than mid, the entire inteval lies
+            #in the right subtree
             elif i >= mid + 1:
-                return sumRange_(root.right, i, j)
-
+                return rangeSum(root.right, i, j)
+            
+            #Otherwise, the interval is split. So we calculate the sum recursively,
+            #by splitting the interval
             else:
-                return sumRange_(root.left, i, mid) + sumRange_(root.right, mid+1, j)
-
-        return sumRange_(self.root, i, j)
-
-
-
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
+                return rangeSum(root.left, i, mid) + rangeSum(root.right, mid+1, j)
         
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
+        return rangeSum(self.root, i, j)
